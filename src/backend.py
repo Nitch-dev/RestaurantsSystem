42a9d64319ebf7617cdd587e8e7c1a6fb1a24d11
Nitch <hassan.rahmani922@gmail.com>
2023-01-25 02:43:50 +0500

    more dynamic

--- src/backend.py
+++ src/backend.py
@@ -3,13 +3,14 @@ import sqlite3
 con = sqlite3.connect("food.db")
 c = con.cursor()
 
-food = input("Food > ")
-price = input("price > ")
+#for now value is null
+food = None #put your food input name var equal to this
+price =  None #put your food input name var equal to this
 
-
+#adds items to database
 def addItemsToDb(food,price):
         c.execute("INSERT INTO food VALUES('{}','{}')".format(food,price))
-
+#show all database
 def showTable():
         c.execute("SELECT * FROM food")
         print(c.fetchall())
