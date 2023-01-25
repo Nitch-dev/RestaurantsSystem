bae9192dc37bbe1e48005059219eda25115ecb97
Nitch <hassan.rahmani922@gmail.com>
2023-01-25 23:46:33 +0500

    menu

--- src/backend.py
+++ src/backend.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+   
 
 def addItemsToDb(food, price):
         con = sqlite3.connect("food.db")
@@ -7,4 +7,12 @@ def addItemsToDb(food, price):
         c.execute("INSERT INTO EMPLOYEE VALUES('{}','{}')".format(food,price))
         con.commit()   
         con.close()
-        
+
+def showMenu():
+        con = sqlite3.connect("food.db")
+        c = con.cursor()
+        c.execute("SELECT * FROM EMPLOYEE")
+        menu = tuple(c.fetchall())
+        con.commit()   
+        con.close()
+        return menu
--- src/func.py
+++ src/func.py
@@ -1,3 +1,4 @@
+
 def validate_name(input):
     if len(input) >= 2:
         return True
